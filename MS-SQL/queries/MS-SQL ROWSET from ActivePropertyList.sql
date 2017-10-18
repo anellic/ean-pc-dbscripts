6b9964fa60a6ce93bfafef56033f953587416a43
Jon <jon.arce@gmail.com>
2017-10-18 15:34:54 -0500

    Delete MS-SQL ROWSET from ActivePropertyList.sql

--- MS-SQL/queries/MS-SQL ROWSET from ActivePropertyList.sql
+++
@@ -1,3 +0,0 @@
-SELECT * 
-FROM OPENROWSET(BULK 'C:\Users\jarce\activepropertylist.txt',FORMATFILE='C:\Users\jarce\bcp_activepropertylist.xml', FIRSTROW = 2) AS bcp_activepropertylist
-ORDER BY bcp_activepropertylist.EanHotelID;
