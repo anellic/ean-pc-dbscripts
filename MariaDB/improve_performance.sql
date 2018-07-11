5cd3175f4572d855918a67402215012dc428c0f4
zamronypj@yahoo.com <zamronypj@yahoo.com>
2018-07-11 11:49:41 +0800

    add index to parentregionlist table

--- MariaDB/improve_performance.sql
+++ MariaDB/improve_performance.sql
@@ -1,1 +1,3 @@
 ALTER TABLE regioncentercoordinateslist ADD INDEX idx_region_name (RegionName);
+ALTER TABLE parentregionlist ADD INDEX idx_region_name (RegionName);
+ALTER TABLE parentregionlist ADD INDEX idx_region_type_region_name (RegionType, RegionName);
