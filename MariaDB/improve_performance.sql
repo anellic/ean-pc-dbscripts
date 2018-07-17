f82f448aa07d74a1f2ffa81ec950c373442b710e
zamronypj@yahoo.com <zamronypj@yahoo.com>
2018-07-17 20:00:17 +0800

    add index to improve performance for searching train station by name

--- MariaDB/improve_performance.sql
+++ MariaDB/improve_performance.sql
@@ -1,3 +1,4 @@
 ALTER TABLE regioncentercoordinateslist ADD INDEX idx_region_name (RegionName);
 ALTER TABLE parentregionlist ADD INDEX idx_region_name (RegionName);
 ALTER TABLE parentregionlist ADD INDEX idx_region_type_region_name (RegionType, RegionName);
+ALTER TABLE trainmetrostationcoordinateslist ADD INDEX idx_region_name (RegionName);
